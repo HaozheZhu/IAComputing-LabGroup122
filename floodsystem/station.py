8f8b523f2411620b897aa892d8fed426d690ff80
Haozhe Zhu <hz403@cam.ac.uk>
2022-01-24 23:01:08 +0000

    test modification

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -10,8 +10,7 @@ for manipulating/modifying station data
 class MonitoringStation:
     """This class represents a river level monitoring station"""
 
-    def __init__(self, station_id, measure_id, label, coord, typical_range,
-                 river, town):
+    def __init__(self, station_id, measure_id, label, coord, typical_range, river, town):
 
         self.station_id = station_id
         self.measure_id = measure_id
