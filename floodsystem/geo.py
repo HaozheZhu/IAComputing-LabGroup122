a9f0be1f4ba3d5abe23a49c4ca3538f2544b198d
Haozhe Zhu <hz403@cam.ac.uk>
2022-01-30 19:46:46 +0000

    Improved Docstring style

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -11,6 +11,7 @@ from .utils import sorted_by_key  # noqa
 def rivers_with_stations(stations): 
     """Given a list of MonitoringStation objects, returns a sorted list of names 
     of rivers with at least 1 monitoring station"""
+
     ans = set([])
     for station in stations: 
         ans.add(station.river)
@@ -20,6 +21,7 @@ def rivers_with_stations(stations):
 def stations_by_river(stations): 
     """Given a list of MonitoringStation objects, returns a dictionary with keys being river names
     and values being a sorted list of the names of the monitoring stations on that river"""
+
     ans = {}
     for station in stations: 
         if station.river not in ans: 
@@ -32,11 +34,13 @@ def stations_by_river(stations):
 
 def getStationNum(item): 
     """Helper function to sort the list of tuples (river name, number of stations) according to number of stations"""
+    
     return item[1]
 
 def rivers_by_station_number(stations, N): 
     """Given a list of MonitoringStation objects and an integer N, returns a list of tuples (river name, number of stations)
     of N rivers with the greatest number of monitoring stations"""
+
     river_stations = stations_by_river(stations)
     ans = [(river, len(river_stations[river])) for river in river_stations]
     ans.sort(key=getStationNum, reverse=True)
