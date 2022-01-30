b71c864ec73360f589e6eafd4761458dd86b2fb5
Haozhe Zhu <hz403@cam.ac.uk>
2022-01-30 18:07:20 +0000

    Task1D

---
+++ Task1D.py
@@ -0,0 +1,17 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_with_stations, stations_by_river
+
+def run(): 
+    stations = build_station_list()
+    ans = rivers_with_stations(stations)
+    ans1 = stations_by_river(stations)
+    print("{:d} stations. First 10 - {}".format(len(ans), ans[:10]))
+    print("River Aire: {}".format(ans1["River Aire"]))
+    print("River Cam: {}".format(ans1["River Cam"]))
+    print("River Thames: {}".format(ans1["River Thames"]))
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,22 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def rivers_with_stations(stations): 
+    ans = set([])
+    for station in stations: 
+        if(station.river not in ans): 
+            ans.add(station.river)
+    ans = sorted(ans)
+    return ans
+
+def stations_by_river(stations): 
+    ans = {}
+    for station in stations: 
+        if(station.river not in ans): 
+            ans[station.river] = [station.name]
+        else: 
+            ans[station.river].append(station.name)
+    for key in ans: 
+        ans[key].sort()
+    return ans
