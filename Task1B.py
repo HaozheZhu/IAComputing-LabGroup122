bc889bef5d1a02401ef0999080dd2eb5bc89f744
Haozhe Zhu <hz403@cam.ac.uk>
2022-02-02 19:13:01 +0000

    corrected syntax error

--- Task1B.py
+++ Task1B.py
@@ -5,8 +5,8 @@ def run():
     '''Requirements for Task 1B'''
     stations = build_station_list()
     p = tuple((52.2053, 0.1218))
-    response = stations_by_distance[:10]
-    print(response)
+    response = stations_by_distance(stations, p)
+    print(response[:10])
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
