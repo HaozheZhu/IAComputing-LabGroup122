3211ea140cd5f8b014cbc3c3baf72b32cfc76de4
Thomas Masding <tjm81@cam.ac.uk>
2022-02-03 21:51:05 +0000

    Ammended function name

--- Task1C.py
+++ Task1C.py
@@ -1,12 +1,12 @@
 from floodsystem.stationdata import build_station_list
-from floodsystem.geo import geo.stations_within_radius
+from floodsystem.geo import stations_within_radius
 
 def run():
     '''Requirements for Task 1B'''
     stations = build_station_list()
     centre = tuple((52.2053, 0.1218))
     r = 10.0
-    response = geo.stations_within_radius(stations, centre, r)
+    response = stations_within_radius(stations, centre, r)
     print(response)
 
 if __name__ == "__main__":
