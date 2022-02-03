521784ccadbad829a00458bf09c5aa04b137fc6d
Thomas Masding <tjm81@cam.ac.uk>
2022-02-03 21:49:38 +0000

    Created task1c

---
+++ Task1C.py
@@ -0,0 +1,14 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import geo.stations_within_radius
+
+def run():
+    '''Requirements for Task 1B'''
+    stations = build_station_list()
+    centre = tuple((52.2053, 0.1218))
+    r = 10.0
+    response = geo.stations_within_radius(stations, centre, r)
+    print(response)
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
