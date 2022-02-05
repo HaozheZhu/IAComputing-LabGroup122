47ddbe983cf5909298b18b2d76ae6249a9163afc
Thomas Masding <tjm81@cam.ac.uk>
2022-02-05 13:10:06 +0000

    Added all task 1 to yml file

--- Task1C.py
+++ Task1C.py
@@ -2,7 +2,7 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 
 def run():
-    '''Requirements for Task 1B'''
+    '''Requirements for Task 1C'''
     stations = build_station_list()
     centre = tuple((52.2053, 0.1218))
     r = 10.0
