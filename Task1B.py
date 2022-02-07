0df8d5905469aab2104a68663ad412f3744ebacd
Thomas Masding <tjm81@cam.ac.uk>
2022-02-07 17:13:50 +0000

    Changed to deal with full station info

--- Task1B.py
+++ Task1B.py
@@ -6,8 +6,11 @@ def run():
     stations = build_station_list()
     p = tuple((52.2053, 0.1218))
     response = stations_by_distance(stations, p)
-    print(response[:10])
-    print(response[10:])
+    response2 = []
+    for i in range(len(response)):
+        response2.append((response[i][0].name, response[i][0].town, response[i][1]))
+    print(response2[:10])
+    print(response2[10:])
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
