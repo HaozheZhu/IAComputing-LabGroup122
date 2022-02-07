d4b569f29c4738e7158501ec8f100e0329a4591c
Thomas Masding <tjm81@cam.ac.uk>
2022-02-07 15:40:03 +0000

    Added tests for Tasks 1B,C

--- test_geo.py
+++ test_geo.py
@@ -13,3 +13,14 @@ def test_stations_by_river():
 def test_rivers_by_station_number(): 
     stations = build_station_list()
     geo.rivers_by_station_number(stations, 1)
+
+def test_stations_by_distance():
+    stations = build_station_list()
+    p = tuple((52.2053, 0.1218))
+    geo.stations_by_distance(stations, p)
+
+def test_stations_within_radius():
+    stations = build_station_list()
+    centre = tuple((52.2053, 0.1218))
+    r = 10.0
+    geo.stations_within_radius(stations, centre, r)
