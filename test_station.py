98c477f989bb2e4114ec0f2661ba8ade55b6f2d3
Thomas Masding <tjm81@cam.ac.uk>
2022-02-07 15:42:09 +0000

    Added test for 1F

--- test_station.py
+++ test_station.py
@@ -4,6 +4,8 @@
 """Unit test for the station module"""
 
 from floodsystem.station import MonitoringStation
+import floodsystem.station as station
+from floodsystem.stationdata import build_station_list
 
 
 def test_create_monitoring_station():
@@ -25,3 +27,7 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_inconsistent_typical_range_stations():
+    stations = build_station_list()
+    station.inconsistent_typical_range_stations(stations)
