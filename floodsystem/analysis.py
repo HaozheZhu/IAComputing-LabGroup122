d757a91f06459962e2f4be736d9cd34b804450c7
Thomas Masding <tjm81@cam.ac.uk>
2022-02-16 17:35:47 +0000

    removing debugging

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -8,7 +8,6 @@ import matplotlib
 
 def polyfit(dates, levels, p):
     x = matplotlib.dates.date2num(dates)
-    print(x)
     y = levels
     p_coeff = np.polyfit(x - x[0], y, p)
     poly = np.poly1d(p_coeff)
