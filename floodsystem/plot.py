56ee6b8cb07fb80a970b9e514f1485d2fbc61b9e
Thomas Masding <tjm81@cam.ac.uk>
2022-02-16 17:22:04 +0000

    added fitting function

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+from floodsystem.analysis import polyfit
 
 def plot_water_levels(station, dates, levels): 
     """Function for ploting graphs of water level against data"""
@@ -23,5 +26,34 @@ def plot_water_levels(station, dates, levels):
 
     plt.show()
 
+def plot_water_level_with_fit(station, dates, levels, p):
+"""Function for ploting graphs of water level against data with line of best fit"""
+    if station.typical_range_consistent(): 
+        high = [station.typical_range[1] for i in levels]
+        low = [station.typical_range[0] for i in levels]
+
+    poly, d0 = polyfit(dates, levels, p)
+    x = matplotlib.dates.date2num(dates)
+    x1 = np.linspace(x[0], x[-1], 50)
+    x2 = matplotlib.dates.num2date(x1)
+    # Plot
+    plt.plot(dates, levels)
+    plt.plot(x1, poly(x2-d0))
+    plt.plot(dates, high, label="Typical High")
+    plt.plot(dates, low, label="Typical Low")
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45)
+    plt.title(station.name)
+    plt.legend()
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    plt.show()
+
 if __name__ == "__main__": 
     plot_water_levels(1, 1, 1)
+
