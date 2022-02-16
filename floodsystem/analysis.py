b6a61c6c1dd4405569c05e2a063c08cde9e7ba10
Thomas Masding <tjm81@cam.ac.uk>
2022-02-16 17:02:40 +0000

    Created the polynomial fit function

---
+++ floodsystem/analysis.py
@@ -0,0 +1,15 @@
+"""This module provides a line of best fit for the historical data of the water levels
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+def polyfit(dates, levels, p):
+    x = matplotlib.dates.date2num(dates)
+    y = levels
+    p_coeff = np.polyfit(x - x[0], y, p)
+    poly = np.poly1d(p_coeff)
+    d0 = x[0]
+    return poly, d0
