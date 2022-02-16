570a4d367db03dccbf682ed0badfa86a3a7ccba6
Thomas Masding <tjm81@cam.ac.uk>
2022-02-16 17:34:02 +0000

    debugging, printing variables

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -8,8 +8,9 @@ import matplotlib
 
 def polyfit(dates, levels, p):
     x = matplotlib.dates.date2num(dates)
+    print(x)
     y = levels
-    p_coeff = np.polyfit(x - x[1], y, p)
+    p_coeff = np.polyfit(x - x[0], y, p)
     poly = np.poly1d(p_coeff)
     d0 = x[1]
     return poly, d0
