df0e7e6eb93596f42603c8296aa70553b5b98fea
Thomas Masding <tjm81@cam.ac.uk>
2022-02-22 14:17:37 +0000

    Created task 2g

---
+++ Task2G.py
@@ -0,0 +1,29 @@
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+import datetime
+from floodsystem.analysis import floodriskgrad
+
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+    severe=[]
+    high=[]
+    moderate=[]
+    low=[]
+    for station in stations:
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=1))
+        if floodriskgrad(dates, levels) == "Severe":
+            severe.append(station.name)
+        elif floodriskgrad(dates, levels) == "High":
+            high.append(station.name)
+        elif floodriskgrad(dates, levels) == "Moderate":
+            moderate.append(station.name)
+        elif floodriskgrad(dates, levels) == "Low":
+            low.append(station.name)
+    print severe 
+
+
+
+if __name__ == "__main__":
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
+    run()
