d9c2f155a40fa408e2866dd1c7e63e475aea77f8
Thomas Masding <tjm81@cam.ac.uk>
2022-02-22 14:00:24 +0000

    added fit line test

--- test_plot.py
+++ test_plot.py
@@ -1,4 +1,4 @@
-from floodsystem.plot import plot_water_levels
+from floodsystem.plot import plot_water_levels, plot_water_level_with_fit
 from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
@@ -8,3 +8,9 @@ def test_plot_water_levels():
     dates, levels = fetch_measure_levels(stations[0].measure_id,
                                                 dt=datetime.timedelta(days=10))
     plot_water_levels(stations[0], dates, levels)
+
+def test_plot_water_levels_with_fit():
+    stations = build_station_list()
+    dates, levels = fetch_measure_levels(stations[0].measure_id,
+                                                dt=datetime.timedelta(days=2))
+    plot_water_level_with_fit(stations[0], dates, levels, 4)
