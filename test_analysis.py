b1f473b4500f21f32235bf883fc1028c9c842db0
Thomas Masding <tjm81@cam.ac.uk>
2022-02-22 13:52:00 +0000

    Created test for analysis

---
+++ test_analysis.py
@@ -0,0 +1,14 @@
+'''File to test Analysis submodule'''
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from floodsystem.stationdata import build_station_list
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.analysis import polyfit
+import datetime
+
+def test_polyfit():
+    stations = build_station_list()
+    update_water_levels(stations)
+    dates, levels = fetch_measure_levels(stations[0].measure_id, dt=datetime.timedelta(days=2))
+    polyfit(dates, levels, 4)
