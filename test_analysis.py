fc2bc03d9b7cada65e0779a19e662a9a8fcb0fd1
Thomas Masding <tjm81@cam.ac.uk>
2022-02-23 17:51:23 +0000

    added 2g to testing

--- test_analysis.py
+++ test_analysis.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 from floodsystem.stationdata import build_station_list, update_water_levels 
 from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.analysis import polyfit
+from floodsystem.analysis import polyfit, floodrisk
 import datetime
 
 def test_polyfit():
@@ -12,3 +12,9 @@ def test_polyfit():
     update_water_levels(stations)
     dates, levels = fetch_measure_levels(stations[0].measure_id, dt=datetime.timedelta(days=2))
     polyfit(dates, levels, 4)
+
+def test_floodrisk():
+    stations = build_station_list()
+    dates, levels = fetch_measure_levels(stations[0].measure_id, dt=datetime.timedelta(days=1))
+    risk = floodrisk(stations[0], dates, levels, 4)
+    assert risk
