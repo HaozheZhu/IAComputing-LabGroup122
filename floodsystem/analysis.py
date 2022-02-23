d4582aa5957577b4fbcb2b0ddf1a408c3789527f
Thomas Masding <tjm81@cam.ac.uk>
2022-02-23 11:32:04 +0000

    Added error correction

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -9,12 +9,15 @@ import matplotlib.pyplot as plt
 
 
 def polyfit(dates, levels, p):
-    x = matplotlib.dates.date2num(dates)
-    y = levels
-    p_coeff = np.polyfit(x - x[0], y, p)
-    poly = np.poly1d(p_coeff)
-    d0 = x[0]
-    return poly, d0
+    try:
+        x = matplotlib.dates.date2num(dates)
+        y = levels
+        p_coeff = np.polyfit(x - x[0], y, p)
+        poly = np.poly1d(p_coeff)
+        d0 = x[0]
+        return poly, d0
+    except:
+        return False, False
 
 def floodrisk(station, dates, levels, p):
     count = 0
@@ -22,23 +25,24 @@ def floodrisk(station, dates, levels, p):
         if station.relative_water_level() != None:
             if station.relative_water_level() <100:
                 poly, d0 = polyfit(dates, levels, p)
-                x = matplotlib.dates.date2num(dates)
-                x1 = np.linspace(x[0], x[-1], 1000)
-                grad = (poly(x1[999]-d0)-poly(x1[0]-d0))
-                if grad > 1.0:
-                    count += 2
-                elif grad > 0.5:
-                    count += 1
-                if station.relative_water_level() > 1:
-                    count += 1
-                if count == 3:
-                    return "Severe"
-                elif count == 2:
-                    return "High"
-                elif count == 1:
-                    return "Moderate"
-                else:
-                    return "Low"
+                if poly != False:
+                    x = matplotlib.dates.date2num(dates)
+                    x1 = np.linspace(x[0], x[-1], 1000)
+                    grad = (poly(x1[999]-d0)-poly(x1[0]-d0))
+                    if grad > 1.0:
+                        count += 2
+                    elif grad > 0.5:
+                        count += 1
+                    if station.relative_water_level() > 1:
+                        count += 1
+                    if count == 3:
+                        return "Severe"
+                    elif count == 2:
+                        return "High"
+                    elif count == 1:
+                        return "Moderate"
+                    else:
+                        return "Low"
             else:
                 return "Low"
         else:
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -33,26 +33,27 @@ def plot_water_level_with_fit(station, dates, levels, p):
         low = [station.typical_range[0] for i in levels]
 
     poly, d0 = polyfit(dates, levels, p)
-    x = matplotlib.dates.date2num(dates)
-    x1 = np.linspace(x[0], x[-1], 1000)
-    x2 = matplotlib.dates.num2date(x1)
-    # Plot
-    plt.plot(dates, levels)
-    plt.plot(x2, poly(x1-d0))
-    plt.plot(dates, high, label="Typical High")
-    plt.plot(dates, low, label="Typical Low")
+    if poly != False:
+        x = matplotlib.dates.date2num(dates)
+        x1 = np.linspace(x[0], x[-1], 1000)
+        x2 = matplotlib.dates.num2date(x1)
+        # Plot
+        plt.plot(dates, levels)
+        plt.plot(x2, poly(x1-d0))
+        plt.plot(dates, high, label="Typical High")
+        plt.plot(dates, low, label="Typical Low")
 
-    # Add axis labels, rotate date labels and add plot title
-    plt.xlabel('date')
-    plt.ylabel('water level (m)')
-    plt.xticks(rotation=45)
-    plt.title(station.name)
-    plt.legend()
+        # Add axis labels, rotate date labels and add plot title
+        plt.xlabel('date')
+        plt.ylabel('water level (m)')
+        plt.xticks(rotation=45)
+        plt.title(station.name)
+        plt.legend()
 
-    # Display plot
-    plt.tight_layout()  # This makes sure plot does not cut off date labels
+        # Display plot
+        plt.tight_layout()  # This makes sure plot does not cut off date labels
 
-    plt.show()
+        plt.show()
 
 if __name__ == "__main__": 
     plot_water_levels(1, 1, 1)
